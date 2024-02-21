b35ba210aa9809e1f09100d07346efe8dc2377c7
Mikhail Pogorelovski <druidich92@gmail.com>
2024-02-21 21:26:16 +0500

    [+] load BE data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React, { type ReactElement } from 'react';
+import React from 'react';
 import './scss/app.scss';
 import { Header, Sort, Categoryes, PizzaBlock } from './components';
-import pizass from './assets/pizzas.json';
-function App(): ReactElement {
+// import pizass from './assets/pizzas.json';
+
+function App(): React.ReactElement {
+  const [items, setItems] = React.useState<any[]>([]);
+  React.useEffect(() => {
+    fetch('https://65d61378f6967ba8e3bd739e.mockapi.io/pizzas')
+      .then(async (res) => await res.json())
+      .then((json: any[]) => {
+        setItems(json);
+      })
+      .catch(() => {});
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -14,7 +25,7 @@ function App(): ReactElement {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizass.map((obj: any) => (
+            {items.map((obj: any) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
